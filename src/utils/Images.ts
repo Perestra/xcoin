186d7cef1c66c6245275bb89f4fe16dbc76a06ab
Perestra <danilo.ps.1996@gmail.com>
2024-10-07 17:22:52 -0300

    Image file with the image's import

---
+++ src/utils/Images.ts
@@ -0,0 +1,13 @@
+import initial1 from '../assets/png/initial1.png'
+import initial2 from '../assets/png/initial2.png'
+import initial3 from '../assets/png/initial3.png'
+import initial4 from '../assets/png/initial4.png'
+
+const images: Record<string, string> = {
+    'initial1': initial1,
+    'initial2': initial2,
+    'initial3': initial3,
+    'initial4': initial4
+};
+
+export default images
