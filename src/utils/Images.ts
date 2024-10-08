be1e6db278ab311f30b1d4d20aef4174924f8c9a
Perestra <danilo.ps.1996@gmail.com>
2024-10-08 18:52:38 -0300

    Changed the const name

--- src/utils/Images.ts
+++ src/utils/Images.ts
@@ -3,11 +3,11 @@ import initial2 from '../assets/png/initial2.png'
 import initial3 from '../assets/png/initial3.png'
 import initial4 from '../assets/png/initial4.png'
 
-const images: Record<string, string> = {
+const initialImages: Record<string, string> = {
     'initial1': initial1,
     'initial2': initial2,
     'initial3': initial3,
     'initial4': initial4
 };
 
-export default images
+export {initialImages};
