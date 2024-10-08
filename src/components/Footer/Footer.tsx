3b81cd94482e87f97043cc955c5ee854d4384e7e
Perestra <danilo.ps.1996@gmail.com>
2024-10-08 18:50:41 -0300

    Created the Footer component and its respective json file

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,20 @@
+import styles from './Footer.module.scss'
+import whiteLogo from '../../assets/svg/white_logo.svg'
+import social from '../..//json/SocialMedia.json'
+
+import { Logo } from '../Logo/Logo'
+import { Link } from 'react-router-dom'
+
+export const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+        <Logo src={whiteLogo} />
+        <div className={styles.footer__media}>
+            {social.Media.map( link => (
+                <Link key={link.id} to={link.url}>{link.name}</Link>
+            ) )}    
+        </div>
+        <span>© XCoin. Desenvolvido por <strong>Danilo Perestrelo</strong></span>
+    </footer>
+  )
+}
