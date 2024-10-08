b6349533ce4a1410ff5dec843cd90a75ec6afffd
Perestra <danilo.ps.1996@gmail.com>
2024-10-08 18:52:23 -0300

    New imports and some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import styles from './Header.module.scss'
+import  LogoColored from '../../assets/svg/colored_logo.svg'
 
 import { Logo } from '../Logo/Logo';
 import { NavLink } from '../NavLink/NavLink';
@@ -15,7 +16,7 @@ export const Header: React.FC<Props> = ({classNav, classUser, classLogin}) => {
   return (
     <header className={styles.header}>
       <div className={styles.header__container}>
-        <Logo />
+        <Logo src={LogoColored}/>
         <NavLink className={classNav}/>
         <User className={classUser}/>
         <span className={`${styles.header__login} ${classLogin}`}><Link to="*" role='link' aria-label='Entre ou cadastre-se' >Entre ou cadastre-se</Link></span>  
--- src/pages/Initial/Initial.tsx
+++ src/pages/Initial/Initial.tsx
@@ -1,11 +1,14 @@
 import styles from './Initial.module.scss'
 import cards from '../../json/InitialCard.json'
-import images from '../../utils/Images'
+import currency from '../../json/CurrencyType.json'
+import line from '../../assets/png/app.png'
 
+import {initialImages} from '../../utils/Images'
+import {currencyType} from '../../utils/Icons'
 import { Header } from '../../components/Header/Header'
 import { Button } from '../../components/Button/Button'
 import { InitialCard } from '../../components/InitialCard/InitialCard'
-
+import { CurrencyType } from '../../components/CurrencyType/CurrencyType'
 
 export const Initial: React.FC = () => {
   return (
@@ -24,7 +27,7 @@ export const Initial: React.FC = () => {
             {cards.Initial.map( card => (
               <InitialCard 
                 key={card.id}  
-                src={images[card.image.url]}
+                src={initialImages[card.image.url]}
                 alt={card.image.alt}
                 title={card.title}
                 description={card.description}
@@ -34,6 +37,32 @@ export const Initial: React.FC = () => {
         </section>
         <section className={styles.initial__section}>
           <h1>Disponibilizamos os mais variados tipos de moeda</h1>
+          <div className={styles.initial__currency}>
+            {currency.Currency.map( type => (
+              <CurrencyType 
+                key={type.id}
+                title={type.title}
+                description={type.description}
+                icon={currencyType[type.icon]}
+              />
+            ) )}
+          </div>
+        </section>
+        <div className={styles.initial__line}>
+          <h1>Venha revolucionar o mundo das finanças conosco!</h1>
+          <Button 
+            text='Abra sua conta!'
+            color='green'
+          />
+        </div>
+        <section className={styles.initial__app}>
+            <div className={styles.initial__imgApp}>
+              <img src={line} alt="Mulher branca e ruiva celular o celular preto com as duas mãos e na mão esquerda está segurando uma sacola vermelha e outra amarela." />
+            </div>
+            <div className={styles.initial__appDescription}>
+              <h1>Aplicativo XCoin</h1>
+              <p>Acompanhe seu saldo, extrato e recarregue seu cartão pré-pago internacional. Pelo App você também recebe notificações quando o Dólar, Euro e outras moedas estrangeiras estiverem em queda para aproveitar a melhor cotação para realizar suas recargas.</p>
+            </div>
         </section>
       </main>
     </div>
