e9463f0621e5aeffa5f0525475e8c025dfd17633
Perestra <danilo.ps.1996@gmail.com>
2024-10-08 18:52:53 -0300

    Added the Footer component in the global Router

--- Routers.tsx
+++ Routers.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Initial } from './src/pages/Initial/Initial'
+import { Footer } from './src/components/Footer/Footer'
 
 export const Routers = () => {
   return (
     <BrowserRouter>
-        <Routes>
-            <Route index element={<Initial />}/>
-        </Routes>
+      <Routes>
+        <Route index element={<Initial />}/>
+      </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
