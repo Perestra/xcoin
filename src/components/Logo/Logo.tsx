523b8f02ef920fd384e079bd1efed1bc556a8de8
Perestra <danilo.ps.1996@gmail.com>
2024-10-08 18:51:43 -0300

    Added a Prop in Logo component

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import styles from './Logo.module.scss'
-import  LogoColored from '../../assets/svg/colored_logo.svg'
 
-export const Logo: React.FC = () => {
+type Props = {
+  src: string;
+}
+
+export const Logo: React.FC<Props> = ({ src }) => {
   return (
     <div className={styles.logo}>
-        <a className={styles.logo__link} href='*'>
-            <img className={styles.logo__img} src={LogoColored} alt="Logo da XCoin com o X em verde e o Coin em Branco" />
-        </a>
+      <a className={styles.logo__link} href='*'>
+        <img className={styles.logo__img} src={src} alt="Logo da XCoin com o X em verde e o Coin em Branco" />
+      </a>
     </div>
   )
 }
