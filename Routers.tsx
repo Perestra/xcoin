f1f486c581376e5529c656b7906dd32a3cc84c67
Perestra <danilo.ps.1996@gmail.com>
2024-10-09 08:24:29 -0300

    Added the SignIn page in Routes

--- Routers.tsx
+++ Routers.tsx
@@ -2,12 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import { Initial } from './src/pages/Initial/Initial'
 import { Footer } from './src/components/Footer/Footer'
+import { SignIn } from './src/pages/SignIn/SignIn'
 
 export const Routers = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Initial />}/>
+        <Route path='signin' element={<SignIn />}/>
       </Routes>
       <Footer />
     </BrowserRouter>
