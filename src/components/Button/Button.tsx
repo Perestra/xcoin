e3b3ff4f0a3533e2416690bfe1b6bf2e458c3055
Perestra <danilo.ps.1996@gmail.com>
2024-10-09 19:48:33 -0300

    Added new props and new styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
+import { Link } from 'react-router-dom';
 import styles from './Button.module.scss'
 
 type Props = {
     icon?: React.ElementType;
     text?: string;
+    path: string;
     color: string;
+    type: "button" | "submit";
 }
 
-export const Button: React.FC<Props> = ({ icon: Icon, text, color }) => {
+export const Button: React.FC<Props> = ({ icon: Icon, text, path, color, type }) => {
   return (
-    <button className={`${styles.button} ${styles[color]}`} type="button">
-        {Icon && <Icon className={styles.button__icon} />}
-        <span>{text}</span>
+    <button className={`${styles.button} ${styles[color]}`} type={type}>
+      {Icon && <Icon className={styles.button__icon} />}
+      {!path && <span>{text}</span>}
+      {path && <Link to={path}>{text}</Link>}
     </button>
   )
 }
