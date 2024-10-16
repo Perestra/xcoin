203af12e187c8e78f51604ba1d44019d2f06b69d
Perestra <danilo.ps.1996@gmail.com>
2024-10-15 22:34:43 -0300

    Added the Provider in Global route

--- Routers.tsx
+++ Routers.tsx
@@ -3,15 +3,18 @@ import { Initial } from './src/pages/Initial/Initial'
 import { SignIn } from './src/pages/SignIn/SignIn'
 import { CreateAccount } from './src/pages/CreateAccount/CreateAccount'
 import { Footer } from './src/components/Footer/Footer'
+import { AccountProvider } from '@/contexts/AccountContext'
 
 export const Routers = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route index element={<Initial />}/>
-        <Route path='signin' element={<SignIn />}/>
-        <Route path='createaccount' element={<CreateAccount />} />
-      </Routes>
+      <AccountProvider>
+        <Routes>
+          <Route index element={<Initial />}/>
+          <Route path='signin' element={<SignIn />}/>
+          <Route path='createaccount' element={<CreateAccount />} />
+        </Routes>  
+      </AccountProvider>
       <Footer />
     </BrowserRouter>
   )
