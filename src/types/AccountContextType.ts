0292eb0f174f29bb7bb8bf7b569d327b73a76b5a
Perestra <danilo.ps.1996@gmail.com>
2024-10-21 19:20:31 -0300

    Added the undefined type in setAccounts

--- src/types/AccountContextType.ts
+++ src/types/AccountContextType.ts
@@ -2,5 +2,5 @@ import { AccountsType } from "./AccountsType";
 
 export type AccountContextType = {
     accounts: AccountsType[];
-    setAccounts: React.Dispatch<React.SetStateAction<AccountsType[]>>
+    setAccounts: React.Dispatch<React.SetStateAction<AccountsType[] | undefined>>
 }
