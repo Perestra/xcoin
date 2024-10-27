82d6aedaa03955768c4a673ff034972a8b77ee25
Perestra <danilo.ps.1996@gmail.com>
2024-10-27 19:46:10 -0300

    Move some files to a new directory

--- src/yupValidations/CreateAccountValidation.ts
+++ src/yupValidations/CreateAccountValidation.ts
@@ -20,12 +20,12 @@ object({
         string()
         .required("É necessário inserir seu usuário.")
         .max(16, "É necessário ter, no máximo, 16 caracteres.")
-        .test('validUsername', "Já existe uma conta com esse usuário", value => !isValidInputData(value, 'username', accounts)),
+        .test('validUsername', "Já existe uma conta com esse usuário.", value => !isValidInputData(value, 'username', accounts)),
     email: 
         string()
         .required("É necessário inserir um e-mail.")
         .email("É necessário digitiar um e-mail válido.")
-        .test('validEmail', "Já existe uma conta com esse e-mail", value => !isValidInputData(value, 'email', accounts)),
+        .test('validEmail', "Já existe uma conta com esse e-mail.", value => !isValidInputData(value, 'email', accounts)),
     password: 
         string()
         .required("É necessário inserir uma senha.")
@@ -37,7 +37,7 @@ object({
         .oneOf([ref("password")], "As senhas devem ser iguais."),
     currency: 
         string()
-        .required("É necessário selecionar sua moeda principal")
+        .required("É necessário selecionar sua moeda principal.")
 })
 
 export { initialValues, CreateAccountSchema }
