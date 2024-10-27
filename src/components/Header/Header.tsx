abf837f2d4bb8c487ebb67dff1e64584738c28c8
Perestra <danilo.ps.1996@gmail.com>
2024-10-27 19:44:58 -0300

    Added a new Props and changed some styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,13 +10,15 @@ type Props = {
   classNav?: string;
   classUser?: string;
   classLogin?: string;
+  bgColor?: string;
+  to: string;
 }
 
-export const Header: React.FC<Props> = ({classNav, classUser, classLogin}) => {
+export const Header: React.FC<Props> = ({bgColor, classNav, classUser, classLogin, to }) => {
   return (
-    <header className={styles.header}>
+    <header className={`${styles.header} ${bgColor}`}>
       <div className={styles.header__container}>
-        <Logo src={LogoColored}/>
+        <Logo src={LogoColored} to={to}/>
         <NavLink className={classNav}/>
         <User className={classUser}/>
         <span className={`${styles.header__login} ${classLogin}`}><Link to="/signin" role='link' aria-label='Entre ou cadastre-se' >Entre ou cadastre-se</Link></span>  
