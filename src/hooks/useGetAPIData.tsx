6b18f8dda21c483e1b1b0cc18ccc384366081284
Perestra <danilo.ps.1996@gmail.com>
2024-10-28 19:04:06 -0300

    Changed the useAxios type

--- src/hooks/useGetAPIData.tsx
+++ src/hooks/useGetAPIData.tsx
@@ -3,7 +3,7 @@ import { AxiosInstance } from "axios"
 
 export const useGetAPIData = (instance: AxiosInstance, url: string) => {
     
-    const { data, loading, error } = useAxios<Record<string, string>>(instance, 'get', url)
+    const { data, loading, error } = useAxios<Record<any, any>>(instance, 'get', url)
 
     return {data, loading, error}
 }
