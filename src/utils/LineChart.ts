a86c1eed6cbe20350dd906f031fff6a50684197d
Perestra <danilo.ps.1996@gmail.com>
2024-10-28 17:34:16 -0300

    Added new parameters

--- src/utils/LineChart.ts
+++ src/utils/LineChart.ts
@@ -15,30 +15,16 @@ ChartJS.register(
     Legend
 )
 
-const lineChartData = {
-    labels: [
-        "11/10/2024",
-        "12/10/2024",
-        "13/10/2024",
-        "14/10/2024",
-        "15/10/2024",
-        "16/10/2024",
-        "17/10/2024",
-        "18/10/2024",
-        "19/10/2024",
-        "20/10/2024",
-        "21/10/2024",
-        "22/10/2024",
-        "23/10/2024",
-        "24/10/2024",
-        "25/10/2024",
-    ],
+const lineChartData = (dates: string[], currencys: number[]) => ({
+    labels: dates.map((date: string) => date),
     datasets: [{
         label: "Valor da moeda",
-        data: [5.65, 5.62, 5.67, 5.61, 5.65, 5.64, 5.59, 5.60, 5.63, 5.65, 5.68, 5.67, 5.63, 5.62, 5.60],
-        borderColor: "#8CC7AB"
-    }],
-}
+        data: currencys.map((currency: number) => currency),
+        borderColor: "#8CC7AB",
+        fill: false,
+        tension: 0.1
+    }]
+})
 
 const lineChartOptions = {
     type: "Line",
