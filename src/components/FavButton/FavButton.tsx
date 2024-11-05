499e674e8529f73346ba11aacd60ac16aee4e095
Perestra <danilo.ps.1996@gmail.com>
2024-11-04 22:37:03 -0300

    Added new prop in FavButton component

--- src/components/FavButton/FavButton.tsx
+++ src/components/FavButton/FavButton.tsx
@@ -1,13 +1,23 @@
+import { useFavoriteCurrency } from '@/hooks/useFavoriteCurrency';
 import { Button } from '../Button/Button'
 import { PiHeart, PiHeartFill } from "react-icons/pi";
 
-export const FavButton = () => {
+type Props = {
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+  code: string;
+}
+
+export const FavButton: React.FC<Props> = ({ onClick, code }) => {
+
+  const { isCodeInList } = useFavoriteCurrency(code)
+
   return (
     <Button  
-        type='button'
-        color='green'
-        text='Favoritar moeda'
-        icon={PiHeart}
+      type='button'
+      color='green'
+      title='Favoritar'
+      icon={!isCodeInList? PiHeart: PiHeartFill}
+      onClick={onClick}
     />
   )
 }
