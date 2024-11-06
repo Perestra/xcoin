8bb4d0430a6311eb8bd2ac59437a6f7cac9d57ce
Perestra <danilo.ps.1996@gmail.com>
2024-11-06 20:06:24 -0300

    Created a funtion to get just the first and second name

--- src/components/UserIntroduction/UserIntroduction.tsx
+++ src/components/UserIntroduction/UserIntroduction.tsx
@@ -6,10 +6,15 @@ import { getCurrencyCode } from '@/utils/getCurrencyCode'
 export const UserIntroduction = () => {
 
     const { authAccount } = useAuthAccountContext()
+    
+    const getName = (name: string) => {
+      const nameSplited = name.split(' ')
+      return `${nameSplited[0]} ${nameSplited[1]}`
+    }
 
   return (
     <div className={styles.info}>
-        <h2>Olá, <strong>{authAccount.accounts[0].fullName}!</strong></h2>
+        <h2>Olá, <strong>{getName(authAccount.accounts[0].fullName)}</strong></h2>
         <h3>Sua moeda principal é <strong>{getCurrencyCode(authAccount.accounts[0].currency, 0)}</strong></h3>  
     </div>
   )
