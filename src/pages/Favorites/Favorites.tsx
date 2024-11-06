e0abdfc35de9ba60b13a1606a5774b408e25eb6a
Perestra <danilo.ps.1996@gmail.com>
2024-11-06 20:07:24 -0300

    Few adjusts in API call and fixed some styles

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -18,12 +18,20 @@ export const Favorites: React.FC = () => {
     const userLogged = accounts.filter(account => account.id === authAccount.accounts[0].id )[0]
     
     const url = `/last/${userLogged.favorites.map(currency => `${currency.code}-${getCurrencyCode(userLogged.currency,0)}`).join(',')}`
-    const { data } = useAxios(AwsomeAPI, 'get', url)
+    
+    const FavList = () => {
+        if(userLogged.favorites.length > 0) {
+            const { data } = useAxios(AwsomeAPI, 'get', url)
+            
+            const FavList = useMemo( () => 
+                Object.entries(data??{}).map( ([key, value]) => ({label: value, value: key}) 
+                ), [data] 
+            )
 
-    let FavList = useMemo( () => 
-        Object.entries(data??{}).map( ([key, value]) => ({label: value, value: key}) 
-        ), [data] 
-    )
+            return FavList
+        }
+    }
+
 
   return (
     <div className={style.content}>
@@ -35,8 +43,18 @@ export const Favorites: React.FC = () => {
         <main className={style.content__main}>
             <section className={style.content__section}>
                 <UserIntroduction />
+                {userLogged.favorites.length <= 0 && <h3>Você não tem nenhuma moeda favorita...</h3>}
                 <div className={style.content__cards}>
-                    {FavList.map( (favorite, index) => (<FavCard key={index} code={favorite.label.code} exchange={`${Number(favorite.label.bid).toFixed(2)} ${favorite.label.codein}`} />) )}
+                    {userLogged.favorites.length >= 1 &&
+                        FavList()?.map( (favorite, index) => (
+                            <FavCard 
+                                key={index} 
+                                coin={(favorite.label.name).split('/')[0]} 
+                                code={favorite.label.code} 
+                                exchange={`${Number(favorite.label.bid).toFixed(2)} ${favorite.label.codein}`} 
+                            />
+                        ))
+                    }
                 </div>
             </section>
         </main>
