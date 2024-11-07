79d363a629c7a202ecda685fbc2c89312351bf5c
Perestra <danilo.ps.1996@gmail.com>
2024-11-07 19:47:21 -0300

    Fews configs in tsconfig file

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,7 +1,13 @@
 import styles from './NavLink.module.scss'
-import links from '@/json/Pages.json'
+import links from '../../json/Pages.json'
 
 import { LinkPage } from '../LinkPage/LinkPage';
+
+type Link = {
+  id: number;
+  name: string;
+  url: string;
+}
 
 type Props = {
     className?: string;
@@ -10,9 +16,9 @@ type Props = {
 export const NavLink: React.FC<Props> = ({className}) => {
   return (
     <nav className={`${styles.nav} ${className}`}>
-        <ul className={styles.nav__ul}>
-          { links.Header.map( page => (<LinkPage key={page.id} to={page.url} name={page.name} />) ) }
-        </ul>
-      </nav>
+      <ul className={styles.nav__ul}>
+        { links.Header.map( (page: Link) => (<LinkPage key={page.id} to={page.url} name={page.name} />) ) }
+      </ul>
+    </nav>
   )
 }
