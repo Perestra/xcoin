f8642fb648001ca8797f960b619031f153d6e460
Perestra <danilo.ps.1996@gmail.com>
2024-11-07 20:36:56 -0300

    Changed the name file and imported in NavLink component

--- src/components/NavLink/NavLink.tsx
+++ src/components/NavLink/NavLink.tsx
@@ -1,5 +1,5 @@
 import styles from './NavLink.module.scss'
-// import links from '../../json/Pages.json'
+import links from '../../json/LinkPages.json'
 
 import { LinkPage } from '../LinkPage/LinkPage';
 
@@ -13,29 +13,29 @@ type Props = {
   className?: string;
 }
 
-const links = [
-  {   
-    "id":1,
-    "name": "Variação",
-    "url": "/variacao"
-  },
-  {   
-    "id":2,
-    "name": "Conversão",
-    "url": "/conversao"
-  },
-  {   
-    "id":3,
-    "name": "Favoritos",
-    "url": "/favoritos"
-  }
-]
+// const links = [
+//   {   
+//     "id":1,
+//     "name": "Variação",
+//     "url": "/variacao"
+//   },
+//   {   
+//     "id":2,
+//     "name": "Conversão",
+//     "url": "/conversao"
+//   },
+//   {   
+//     "id":3,
+//     "name": "Favoritos",
+//     "url": "/favoritos"
+//   }
+// ]
 
 export const NavLink: React.FC<Props> = ({className}) => {
   return (
     <nav className={`${styles.nav} ${className}`}>
       <ul className={styles.nav__ul}>
-        { links.map( (page: Link) => (<LinkPage key={page.id} to={page.url} name={page.name} />) ) }
+        { links.Header.map( (page: Link) => (<LinkPage key={page.id} to={page.url} name={page.name} />) ) }
       </ul>
     </nav>
   )
